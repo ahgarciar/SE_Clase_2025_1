1e14bfda7853568c7ab7d0db177d3cc9d1c329d2
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2025-03-26 17:45:32 -0600

    update

--- Unidad_3/A4_Busqueda_Tabu/main.py
+++ Unidad_3/A4_Busqueda_Tabu/main.py
@@ -1,15 +1,22 @@
 
 import random as rnd
 #rnd.seed(5)
-xmin = -10
-xmax = 10
+xmin = -100
+xmax = 100
+
+lista_tabu = {}
+tiempo_tabu = 2
 
 def crea_solucion_vecina(solucion):
     vector = solucion[:]
+
     index = rnd.randint(0, len(solucion)-1)
+    while index in lista_tabu:
+        index = rnd.randint(0, len(solucion) - 1)
+
     nuevo_valor = rnd.randint(xmin, xmax)
     vector[index] = nuevo_valor
-    return vector
+    return vector, index
 
 def calcula_fo(solucion):
     vo = sum([i**2 for i in solucion])
@@ -23,8 +30,11 @@ def perturbacion(solucion): #modificacion brusca de la solucion
     vector = solucion[:]
 
     index1 = rnd.randint(0, len(solucion) - 1)
+    while index1 in lista_tabu:
+        index1 = rnd.randint(0, len(solucion) - 1)
+
     index2 = index1  ####
-    while index2 == index1:
+    while index2 == index1 and index2 in lista_tabu:
         index2 = rnd.randint(0, len(solucion) - 1)
 
     nuevo_valor1 = rnd.randint(xmin, xmax)
@@ -33,7 +43,7 @@ def perturbacion(solucion): #modificacion brusca de la solucion
     vector[index1] = nuevo_valor1
     vector[index2] = nuevo_valor2
 
-    return vector
+    return vector, index1, index2
 
 
 if __name__ == "__main__":
@@ -44,7 +54,7 @@ if __name__ == "__main__":
     best_vo = calcula_fo(best_solucion)
     print("solucion vo inicial: ", best_vo)
 
-    max_it_ils = 100  #iterated local search = ils
+    max_it_ils = 1000  #iterated local search = ils
     it_ils = 0
 
     max_it_local = 10000
@@ -53,7 +63,7 @@ if __name__ == "__main__":
     while it_ils < max_it_ils: #busqueda local iterada
 
         while it_local < max_it_local: #busqueda local
-            solucion_temporal = crea_solucion_vecina(solucion_temporal)
+            solucion_temporal, idx1 = crea_solucion_vecina(solucion_temporal)
             vo_temporal = calcula_fo(solucion_temporal)
 
             if vo_temporal < best_vo:
@@ -61,19 +71,42 @@ if __name__ == "__main__":
                 best_solucion = solucion_temporal[:]
                 print("nueva best solucion: ", solucion_temporal, end="    ")
                 print("vo: ", vo_temporal)
+                lista_tabu[idx1] = tiempo_tabu + 1 #agrega el indice a la lista
 
             #print("it", end="   ")
             #print("solucion: ", solucion_temporal, end="    ")
             #print("vo: ", vo_temporal)
+
+            # update tabu times
+            lista_tabu_temp = lista_tabu.copy()
+            for key in lista_tabu.keys():
+                lista_tabu_temp[key] -= 1
+                if lista_tabu_temp[key] == 0:
+                    del lista_tabu_temp[key]
+            lista_tabu = lista_tabu_temp
+            ###
+
             it_local+=1
 
-        solucion_temporal = perturbacion(solucion_temporal)
+        solucion_temporal, idx1, idx2 = perturbacion(solucion_temporal)
         vo_temporal = calcula_fo(solucion_temporal)
         if vo_temporal < best_vo:
             best_vo = vo_temporal
             best_solucion = solucion_temporal[:]
             print("nueva best solucion: ", solucion_temporal, end="    ")
             print("vo: ", vo_temporal)
+            lista_tabu[idx1] = tiempo_tabu + 1 # agrega el indice a la lista
+            lista_tabu[idx2] = tiempo_tabu + 1  # agrega el indice a la lista
+
+        #update tabu times
+        lista_tabu_temp = lista_tabu.copy()
+        for key in lista_tabu.keys():
+            lista_tabu_temp[key] -= 1
+            if lista_tabu_temp[key] == 0:
+                del lista_tabu_temp[key]
+        lista_tabu = lista_tabu_temp
+        ###
+
         it_ils +=1
 
     print("mejor solucion: ", best_solucion)
